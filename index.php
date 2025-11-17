15310e037f2aff5ac449a00b42203b34d51ae56e
Widyasyafitri <widiasafitri9917@gmail.com>
2025-11-17 21:45:21 +0700

    Mendesain Halaman Login

---
+++ index.php
@@ -0,0 +1,6 @@
+<form method="post">
+    Username: <input type="text" name="username" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+    <button type="submit">Login</button>
+    <button type="reset">Batal</button>
+</form>
