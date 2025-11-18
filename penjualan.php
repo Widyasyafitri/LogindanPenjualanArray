513bde7a261f3630001110191ab290398d6f6164
Widyasyafitri <widiasafitri9917@gmail.com>
2025-11-18 11:03:52 +0700

    Dashboard Penjualan(Logika random penjualan)

--- penjualan.php
+++ penjualan.php
@@ -1,26 +1,33 @@
 <?php
-// COMMIT 5 — Array Produk
+// COMMIT 5 — Array Produk (TIDAK DITAMPILKAN DALAM TABEL)
 $kode_barang  = ["P001", "P002", "P003", "P004", "P005"];
 $nama_barang  = ["Sprite", "CocaCola B", "Fanta", "Golda", "Aqua"];
 $harga_barang = [10000, 8000, 7000, 4000, 5000];
 ?>
+
 <h2> POLGAN MART </h2>
 <p>Daftar Pembelian:</p>
 
+<?php
+// COMMIT 6 — Logika Pembelian Random
+$beli   = [];
+$jumlah = [];
+
+for ($i = 0; $i < 5; $i++) {
+    $randomIndex = rand(0, count($kode_barang) - 1);
+    $beli[]   = $kode_barang[$randomIndex];
+    $jumlah[] = rand(1, 10);
+}
+?>
+
 <table border="1" cellpadding="10">
     <tr>
-        <th>Kode Barang</th>
-        <th>Nama Barang</th>
+        <th>Kode</th>
+        <th>Nama</th>
         <th>Harga</th>
+        <th>Jumlah</th>
+        <th>Total</th>
     </tr>
 
     <?php
-    for ($i = 0; $i < count($kode_barang); $i++) {
-        echo "<tr>
-                <td>{$kode_barang[$i]}</td>
-                <td>{$nama_barang[$i]}</td>
-                <td>Rp " . number_format($harga_barang[$i]) . "</td>
-              </tr>";
-    }
-    ?>
-</table>
+    
